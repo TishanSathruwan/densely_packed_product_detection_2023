d60401b3c3b28c6d9cae80875b4b5a8380c0e791
Dojitha Mirihagalla <126095827+dojitha-mirihagalla@users.noreply.github.com>
2023-10-19 09:18:48 +0530

    Update test.py

--- Edits_dojitha/test.py
+++ Edits_dojitha/test.py
@@ -1,1 +1,13 @@
-print("hello world")
+import cv2
+from matplotlib import pyplot as plt
+
+
+
+path = '/kaggle/input/sku110k-annotations/SKU110K_fixed/images/train/train_1039.jpg'
+
+# Read image
+img = cv2.imread(path)
+
+#plot image
+plt.imshow(img)
+plt.show()
